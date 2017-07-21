a510167fc98463a063e495e4a8a720241fcadd2c
Francisco Memoli Olmos <francisco.memoli@gmail.com>
2017-07-21 14:35:48 -0300

    Crear partida

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,17 @@
 angular.module('starter.controllers', [])
 
 .controller('HomeCtrl', ['$scope', 'appServices', function($scope, appServices) {
-	debugger
-	$scope.createGame = function(){
-		debugger
-		appServices.createGame($scope.username, $scope.name);
-	}
 }])
 
 .controller('lobbyCtrl', function($scope) {})
 
 .controller('juegoCtrl', function($scope) {})
 
-.controller('crearPartidaCtrl', function($scope) {
-
-})
+.controller('crearPartidaCtrl', ['$scope', 'appServices', function($scope, appServices) {
+	$scope.createGame = function(){
+		appServices.createGame($scope.username, $scope.name);
+	}
+}])
 
 
 .controller('unirsePartidaCtrl', function($scope) {
